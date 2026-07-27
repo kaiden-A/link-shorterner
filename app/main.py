5cf1ff1b8d9bcd50c67c944cb619e0ee2fc9849f
kaiden-A <amirikhwanfaisal@gmail.com>
2026-07-27 20:46:34 +0800

    change root docs and test cd pipeline

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.database import engine, Base
 from app.routers import links as links_router
 from app.routers import redirect as redirect_router
 
-app = FastAPI(title="Link Shortener", version="0.1.0")
+app = FastAPI(title="Link Shortener", version="0.1.0", docs_url="/api/v1/link-shorterner/docs", openapi_url="/api/v1/link-shorterner/openapi.json")
 
 # Mount both routers under the same prefix
 app.include_router(links_router.router, prefix="/api/v1/link-shorterner")
